6ff184d2e90c7fc68764e37508aae132c02768e7
DaniMasterLucky <danielsilvaloew@gmail.com>
2023-05-15 21:36:46 -0300

    Atividades

---
+++ intex.js
@@ -0,0 +1,62 @@
+
+//atividade 1 
+
+// let segundos = Number(prompt("digite um valor em segundos"))
+
+// const horas = Math.floor (segundos/3600)
+// segundos = segundos % 3600
+
+// const min = Math.floor (segundos/50)
+               /* serve para arredondar.*/
+// segundos = segundos % 60
+
+// console.log(`${horas}:${min}:${segundos}`)
+
+
+//atividade 2
+
+// for (var x=1; x <= 100; x++) {
+//     var skip = 0;
+//     if (x % 3 == 0) {
+//         document.write('Fizz');
+//         skip = 1;
+//     }
+//     if (x % 5 == 0) {
+//         document.write('Buzz');
+//         skip = 1;
+//     }
+//     if (!skip) {
+//         document.write(x);
+//     }
+//     document.write('<br>')
+// }
+
+//atividade 3
+
+// let linhas = Number(prompt("digite numero de linhas"))
+// let contagem = 0
+// for (linhas > 0; linhas--;){
+//     let contagem1 = contagem + 1
+//     let contagem2 = contagem1 + 1
+//     let contagem3 = contagem2 + 1
+//     contagem = contagem + 4
+// console.log(contagem1 , contagem2 , contagem3 ,`pum`)
+// }
+
+
+//atividade 4
+
+
+// let inter = 0
+// let gremio = 0
+// let empates = 0
+// let x = 0
+
+// while (x==0){
+//    inter = inter + Number(prompt("gols do inter")),
+//    gremio = gremio + Number(prompt("gols do gremio")),
+//    empates = empates + Number(prompt("empate")),
+//    x = Number(prompt("digite 0 se quer mais um, digite 2 para"))
+// }
+// console.log('gol do inter',inter,"gol do gremio",gremio,"empates",empates)
+
